4e044299696d3448d8a7ca8a1312897daec4bcdd
kiryto <doanquocthai.it@gmail.com>
2018-10-29 17:39:50 +0700

    save data to localStorage

--- src/app/shared/data/common-info.ts
+++ src/app/shared/data/common-info.ts
@@ -1,7 +1,43 @@
 import {PageTitle} from "../entity/page-title";
+import {LocalstorageKey} from "../utility/localstorage-key";
 
 export class CommonInfo {
+	
+	static initialize() {
+		let localData: string = localStorage.getItem(LocalstorageKey.COMMON_INFO);
+		if(localData) {
+			let commonInfoJson: CommonInfoJSON = Object.assign(new CommonInfoJSON(),JSON.parse(localData));
+			this.IS_LOGIN = commonInfoJson.isLogin;
+			this.TOKEN = commonInfoJson.token;
+		}
+	}
+	
 	static PAGE_TITLE: PageTitle = new PageTitle('Welcome to E-learning', 'Page', '/bg-parallax1.jpg');
 	static IS_LOGIN = false;
 	static TOKEN = "";
+	
+	constructor() {
+	}
+}
+
+export class CommonInfoJSON {
+	private _isLogin: boolean;
+	private _token: string;
+	
+	
+	get isLogin(): boolean {
+		return this._isLogin;
+	}
+	
+	set isLogin(value: boolean) {
+		this._isLogin = value;
+	}
+	
+	get token(): string {
+		return this._token;
+	}
+	
+	set token(value: string) {
+		this._token = value;
+	}
 }
